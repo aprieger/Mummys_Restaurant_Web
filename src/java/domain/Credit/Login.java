5a3d9b7a5befdf2fe68689f637ed80c578aa7f8f
Ron Lawton <rlawton1234@yahoo.com>
2018-01-30 21:28:57 -0700

    login and admin/worker domain

---
+++ src/java/domain/Credit/Login.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author syntel
+ */
+public class Login {
+
+    private String loginId;
+    private String password;
+    private int userType;
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+    
+    
+}
---
+++ src/java/domain/Credit/Worker.java
@@ -0,0 +1,96 @@
+
+package domain;
+/*
+    This class is used to create a worker object.
+    
+*/
+
+public class Worker {
+    private String firstName;
+    private String lastName;
+    private String loginId;
+    private String gender;
+    private String phoneNumber;
+    private int employeeId;
+    private int isAdmin;
+    private int isActive;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLoginId() {
+        return loginId;
+    }
+
+    public void setLoginId(String loginId) {
+        this.loginId = loginId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(int isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public int getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(int isActive) {
+        this.isActive = isActive;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" + "firstName=" + firstName + ", lastName=" + lastName + ", loginId=" + loginId + ", gender=" + gender + ", phoneNumber=" + phoneNumber + ", employeeId=" + employeeId + ", isAdmin=" + isAdmin + ", isActive=" + isActive + ", email=" + email + '}';
+    }
+   
+}
