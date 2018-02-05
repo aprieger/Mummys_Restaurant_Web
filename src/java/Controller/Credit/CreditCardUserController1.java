2c3cfd5cbabe381af7d3d4b6c442a2687a24d67c
Michael Nolan <mike.nolan.joshua@gmail.com>
2018-02-05 16:53:07 -0700

    updated credit

--- src/java/Controller/Credit/CreditCardUserController1.java
+++ src/java/Controller/Credit/CreditCardUserController1.java
@@ -1,11 +1,21 @@
+/*
+This class is the controller for CreditCardController1.jsp
+This controller allows the User to add a new credit card to the site
+The controller then redirects to the next page in the order process. 
+*/
+
+
 package Controller.Credit;
 import DAO.Credit.CreditCardDAOImpl;
 //import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
 import domain.Credit.CreditCard;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 //import javax.servlet.http.HttpServletRequest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.validation.BindException;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 //import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,18 +32,23 @@ public class CreditCardUserController1 extends SimpleFormController {
     }
 
     @Override
-    public ModelAndView onSubmit(Object command) throws Exception{
+    public ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command, BindException errors) throws Exception {
       
+        HttpSession session;
+        session = request.getSession(true);
+        
         CreditCard card = (CreditCard) command;
+        card.setCUSTOMER_ID(Integer.parseInt(session.getAttribute("customerID").toString()));
         System.out.println(card.toString());
         ApplicationContext ctx=new ClassPathXmlApplicationContext("../../WEB-INF/applicationContext.xml");
         CreditCardDAOImpl cc=(CreditCardDAOImpl)ctx.getBean("CreditCardDAOImpl");
         cc.addRecord(card); 
-        HttpSession session;
-        //session = request.getSession(true);
-        //session.setAttribute("MySessionVariable", param);
-        //session.setAttribute("card", card);
-        return new ModelAndView("CreditCardUserUI","card",card);
+        
+        
+        
+        session.setAttribute("creditID", card.getCREDIT_ID());
+        session.setAttribute("paymentType", 1);
+        return new ModelAndView("confirmationPage","card",card);
     }
     
 }
