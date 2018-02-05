fdd44110f7209425253a4cc063fd3b8f1b49d0a3
Michael Nolan <mike.nolan.joshua@gmail.com>
2018-02-05 16:56:28 -0700

    updated credit

--- src/java/Validator/Credit/CreditCardUser2Validator.java
+++ src/java/Validator/Credit/CreditCardUser2Validator.java
@@ -7,7 +7,7 @@ import domain.Credit.CreditCard;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
-public class CreditCardAdmin2Validator implements Validator {
+public class CreditCardUser2Validator implements Validator {
     @Override
 	public boolean supports(Class<?> clazz) {
             return true;
